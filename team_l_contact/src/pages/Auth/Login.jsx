c5b6b055cdca01d9002373240964de2d74e5ad91
Khant-Nyar-Ko-Ko <kokhantnyar19@gmail.com>
2023-06-10 14:50:03 +0630

    login register responsive

--- team_l_contact/src/pages/Auth/Login.jsx
+++ team_l_contact/src/pages/Auth/Login.jsx
@@ -50,117 +50,118 @@ const Login = () => {
   };
 
   return (
-    <div>
-      <div className="flex items-center justify-center gap-10 bg-white h-screen md:p-5 md:px-10 w-full">
-        {/* left side start  */}
-        <div className=" w-full md:w-1/2 md:p-5 rounded-lg h-full bg-[#f3f5f9]">
-          <div className=" md:px-10 px-5">
-            {/* <span className=" px-1 text-[#3c37ff] text-3xl">Logo</span> */}
-            <div className=" my-3 flex items-center justify-center flex-col">
-              <h1 className=" text-2xl md:text-3xl mt-5 font-bold text-slate-700">
-                Login
-              </h1>
-              <p className="my-3  font-semibold text-slate-400">
-                Welcome back , Chief!
-              </p>
+    <div className="flex items-center justify-center bg-white h-screen p-5 md:px-10">
+      {/* left side start  */}
+      <div className="w-full md:w-1/2 p-5 rounded-lg bg-[#f3f5f9]">
+        <div className="px-10">
+          {/* <span className="px-1 text-[#3c37ff] text-3xl">Logo</span> */}
+          <div className="my-3 flex items-center justify-center flex-col">
+            <h1 className="text-2xl md:text-3xl mt-5 font-bold text-slate-700">
+              Login
+            </h1>
+            <p className="my-3 font-semibold text-slate-400">
+              Welcome back, Chief!
+            </p>
+          </div>
+          <div className="text-sm">
+            <div className="mt-2">
+              <label htmlFor="" className="text-base-300">
+                Email
+              </label>
+              <br />
+              <input
+                value={email}
+                autoFocus
+                onChange={(e) => setEmail(e.target.value)}
+                type="email"
+                placeholder="example@gmail.com"
+                className="border-none bg-white w-full my-2 focus:outline-blue-700 py-2 px-3 rounded-lg"
+              />
             </div>
-            <div className="text-sm">
-              <div className=" mt-2">
-                <label htmlFor="" className=" text-base-300">
-                  Email
+            <div className="mt-2">
+              <div className="flex items-center justify-between">
+                <label htmlFor="" className="text-base-300">
+                  Password
                 </label>
-                <br />
-                <input
-                  value={email}
-                  autoFocus
-                  onChange={(e) => setEmail(e.target.value)}
-                  type="email"
-                  placeholder="example@gmail.com"
-                  className=" border-none bg-white  w-full my-2 focus:outline-blue-700 py-2 px-3 rounded-lg"
-                />
+                {hidePassword ? (
+                  <button onClick={() => setHidePassword(!hidePassword)}>
+                    <AiOutlineEyeInvisible className="text-xl" />
+                  </button>
+                ) : (
+                  <button onClick={() => setHidePassword(!hidePassword)}>
+                    <AiOutlineEye className="text-xl" />
+                  </button>
+                )}
               </div>
-              <div className=" mt-2">
-                <div className="flex items-center justify-between">
-                  <label htmlFor="" className=" text-base-300">
-                    Password
-                  </label>
-                  {hidePassword ? (
-                    <button onClick={() => setHidePassword(!hidePassword)}>
-                      <AiOutlineEyeInvisible className="text-xl" />
-                    </button>
-                  ) : (
-                    <button onClick={() => setHidePassword(!hidePassword)}>
-                      <AiOutlineEye className=" text-xl" />
-                    </button>
-                  )}
-                </div>
-
-                <input
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  type={hidePassword ? "password" : "text"}
-                  placeholder="****"
-                  className=" border-none bg-white  w-full my-2 focus:outline-blue-700 py-2 px-3 rounded-lg"
-                />
-                <span className=" text-end w-full text-sm text-[#3c37ff] hover:underline cursor-pointer">
-                  Recover Password
-                </span>
-              </div>
-              <button
-                className="border-none  w-full my-5 bg-[#3c37ff] text-slate-300 focus:outline-blue-700 py-2 px-3 rounded-lg"
-                onClick={() => loginHandler(user)}
-              >
-                {isLoading ? <Loader className="block mx-auto text-sm"/> : "Login"}
-                {/* Login */}
-              </button>
-              <span className="text-sm font-normal text-slate-400 mt-5">
-                Do not have an account?
+              <input
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                type={hidePassword ? "password" : "text"}
+                placeholder="****"
+                className="border-none bg-white w-full my-2 focus:outline-blue-700 py-2 px-3 rounded-lg"
+              />
+              <span className="text-end w-full text-sm text-[#3c37ff] hover:underline cursor-pointer">
+                Recover Password
               </span>
-              <Link to="/register">
-                <span className="text-[#3c37ff] text-sm mx-3 hover:underline cursor-pointer">
-                  Register
-                </span>
-              </Link>
             </div>
-            <div className=" mt-[20px]">
-              <div className=" flex items-center justify-start gap-3 text-sm text-slate-500">
-                <span className="text-[#3c37ff] ">
-                  <AiOutlineMail />
-                </span>
-                <span>
-                  Need help or suggest anything{" "}
-                  <Link to="/" className="text-[#3c37ff] hover:underline ">
-                    here
-                  </Link>
-                </span>
-              </div>
+            <button
+              className="border-none w-full my-5 bg-[#3c37ff] text-slate-300 focus:outline-blue-700 py-2 px-3 rounded-lg"
+              onClick={() => loginHandler(user)}
+            >
+              {isLoading ? (
+                <Loader className="block mx-auto text-sm" />
+              ) : (
+                "Login"
+              )}
+              {/* Login */}
+            </button>
+            <span className="text-sm font-normal text-slate-400 mt-5">
+              Do not have an account?
+            </span>
+            <Link to="/register">
+              <span className="text-[#3c37ff] text-sm mx-3 hover:underline cursor-pointer">
+                Register
+              </span>
+            </Link>
+          </div>
+          <div className="mt-[20px]">
+            <div className="flex items-center justify-start gap-3 text-sm text-slate-500">
+              <span className="text-[#3c37ff]">
+                <AiOutlineMail />
+              </span>
+              <span>
+                Need help or suggest anything{" "}
+                <Link to="/" className="text-[#3c37ff] hover:underline">
+                  here
+                </Link>
+              </span>
             </div>
           </div>
         </div>
-        {/* left side end  */}
-
-        {/* right side start  */}
-        <div className="md:w-1/2 hidden md:block h-full ">
-          <img src={loginIamge} alt="" />
-        </div>
-
-        {/* right side end  */}
-        {/* React Toast  */}
-        <ToastContainer
-          position="top-right"
-          autoClose={5000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme="light"
-        />
-
-        <ToastContainer />
       </div>
+      {/* left side end  */}
+
+      {/* right side start  */}
+      <div className="w-full md:w-1/2 hidden md:block">
+        <img src={loginIamge} alt="" />
+      </div>
+      {/* right side end  */}
+
+      {/* React Toast  */}
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+
+      <ToastContainer />
     </div>
   );
 };
--- team_l_contact/src/pages/Auth/Register.jsx
+++ team_l_contact/src/pages/Auth/Register.jsx
@@ -52,142 +52,140 @@ const navigate = useNavigate()
     // }
   };
   return (
-    <div>
-      <div className="flex items-center justify-center gap-10 bg-white h-screen md:p-5 md:px-10 w-full">
-        {/* left side start  */}
-        <div className="md:w-1/2 w-full p-1 rounded-lg h-full bg-[#f3f5f9]">
-          <div className=" md:px-10 px-5">
-            {/* <span className=" px-1 text-[#3c37ff] text-3xl">Logo</span> */}
-            <div className=" my-2 flex items-center justify-center flex-col">
-              <h1 className=" text-2xl md:text-3xl mt-3 font-bold text-slate-700">
-                Create an account
-              </h1>
-              <p className="my-3 font-semibold text-sm text-slate-400">
-                Sign up now and unlock exclusive access!
-              </p>
+    <div className="flex items-center justify-center bg-white h-screen p-5 md:px-10">
+      {/* left side start  */}
+      <div className="w-full md:w-1/2 p-1 rounded-lg h-full bg-[#f3f5f9]">
+        <div className="px-10">
+          {/* <span className="px-1 text-[#3c37ff] text-3xl">Logo</span> */}
+          <div className="my-2 flex items-center justify-center flex-col">
+            <h1 className="text-2xl md:text-3xl mt-3 font-bold text-slate-700">
+              Create an account
+            </h1>
+            <p className="my-3 font-semibold text-sm text-slate-400">
+              Sign up now and unlock exclusive access!
+            </p>
+          </div>
+          <div className="text-sm">
+            <div className="mt-2">
+              <label htmlFor="" className="text-base-300">
+                Your Name
+              </label>
+              <br />
+              <input
+                onChange={(e) => setName(e.target.value)}
+                type="text"
+                placeholder="First Last"
+                className="border-none bg-white w-full my-2 focus:outline-blue-700 py-2 px-3 rounded-lg"
+              />
             </div>
-            <div className=" text-sm">
-              <div className=" mt-2">
+            <div className="mt-2">
+              <label htmlFor="" className="text-base-300">
+                Email
+              </label>
+              <br />
+              <input
+                onChange={(e) => setEmail(e.target.value)}
+                type="email"
+                placeholder="example@gmail.com"
+                className="border-none bg-white w-full my-2 focus:outline-blue-700 py-2 px-3 rounded-lg"
+              />
+            </div>
+            <div className="mt-2">
+              <div className="flex items-center justify-between">
                 <label htmlFor="" className="text-base-300">
-                  Your Name
+                  Password
                 </label>
-                <br />
-                <input
-                  onChange={(e) => setName(e.target.value)}
-                  type="text"
-                  placeholder="First Last"
-                  className=" border-none bg-white  w-full my-2 focus:outline-blue-700 py-2 px-3 rounded-lg"
-                />
+                <span className="text-[#3c37ff] text-sm">
+                  {password.length > 8 || password.includes("@")
+                    ? "strong"
+                    : "weak"}
+                </span>
               </div>
-              <div className=" mt-2">
+              <input
+                onChange={(e) => setPassword(e.target.value)}
+                type={hidePassword ? "password" : "text"}
+                placeholder="****"
+                className="border-none bg-white w-full my-2 focus:outline-blue-700 py-2 px-3 rounded-lg"
+              />
+            </div>
+            <div className="mt-2">
+              <div className="flex items-center justify-between">
                 <label htmlFor="" className="text-base-300">
-                  Email
+                  Confirm Password
                 </label>
-                <br />
-                <input
-                  onChange={(e) => setEmail(e.target.value)}
-                  type="email"
-                  placeholder="example@gmail.com"
-                  className=" border-none bg-white  w-full my-2 focus:outline-blue-700 py-2 px-3 rounded-lg"
-                />
-              </div>
-              <div className=" mt-2">
-                <div className=" flex items-center justify-between">
-                  <label htmlFor="" className="text-base-300">
-                    Password
-                  </label>
-                  <span className=" text-[#3c37ff] text-sm">
-                    {password.length > 8 || password.includes("@")
-                      ? "strong"
-                      : "weak"}
-                  </span>
-                </div>
-                <input
-                  onChange={(e) => setPassword(e.target.value)}
-                  type={hidePassword ? "password" : "text"}
-                  placeholder="****"
-                  className=" border-none bg-white w-full my-2 focus:outline-blue-700 py-2 px-3 rounded-lg"
-                />
-              </div>
-              <div className=" mt-2">
-                <div className=" flex items-center justify-between">
-                  <label htmlFor="" className=" text-base-300">
-                    Comfirm Password
-                  </label>
-                  {hidePassword ? (
-                    <button onClick={() => setHidePassword(!hidePassword)}>
-                      <AiOutlineEyeInvisible className="text-xl" />
-                    </button>
-                  ) : (
-                    <button onClick={() => setHidePassword(!hidePassword)}>
-                      <AiOutlineEye className=" text-xl" />
-                    </button>
-                  )}
-                </div>
-                <input
-                  onChange={(e) => setConfirmed_password(e.target.value)}
-                  type={hidePassword ? "password" : "text"}
-                  placeholder="****"
-                  className=" border-none bg-white  w-full my-2 focus:outline-blue-700 py-2 px-3 rounded-lg"
-                />
-              </div>
-              <button
-                className="border-none  w-full my-2 bg-[#3c37ff] text-slate-300 focus:outline-blue-700 py-2 px-3 rounded-lg"
-                onClick={() => registerHandler(user)}
-              >
-                {isLoading ? (
-                  <Loader className="block mx-auto text-sm" />
+                {hidePassword ? (
+                  <button onClick={() => setHidePassword(!hidePassword)}>
+                    <AiOutlineEyeInvisible className="text-xl" />
+                  </button>
                 ) : (
-                  "Create"
+                  <button onClick={() => setHidePassword(!hidePassword)}>
+                    <AiOutlineEye className="text-xl" />
+                  </button>
                 )}
-              </button>
-              <span className="text-sm font-normal text-slate-400 mt-3">
-                Already have an account?
-              </span>
-              <Link to="/login">
-                <span className="text-[#3c37ff] text-sm mx-3 hover:underline cursor-pointer">
-                  Login
-                </span>
-              </Link>
-            </div>
-            <div className="mt-[10px]">
-              <div className=" flex items-center justify-start gap-3 text-sm text-slate-500">
-                <span className="text-[#3c37ff] ">
-                  <AiOutlineMail />
-                </span>
-                <span>
-                  Need help or suggest anything{" "}
-                  <Link to="/" className="text-[#3c37ff] hover:underline ">
-                    here
-                  </Link>
-                </span>
               </div>
+              <input
+                onChange={(e) => setConfirmed_password(e.target.value)}
+                type={hidePassword ? "password" : "text"}
+                placeholder="****"
+                className="border-none bg-white w-full my-2 focus:outline-blue-700 py-2 px-3 rounded-lg"
+              />
+            </div>
+            <button
+              className="border-none w-full my-2 bg-[#3c37ff] text-slate-300 focus:outline-blue-700 py-2 px-3 rounded-lg"
+              onClick={() => registerHandler(user)}
+            >
+              {isLoading ? (
+                <Loader className="block mx-auto text-sm" />
+              ) : (
+                "Create"
+              )}
+            </button>
+            <span className="text-sm font-normal text-slate-400 mt-3">
+              Already have an account?
+            </span>
+            <Link to="/login">
+              <span className="text-[#3c37ff] text-sm mx-3 hover:underline cursor-pointer">
+                Login
+              </span>
+            </Link>
+          </div>
+          <div className="mt-10">
+            <div className="flex items-center justify-start gap-3 text-sm text-slate-500">
+              <span className="text-[#3c37ff]">
+                <AiOutlineMail />
+              </span>
+              <span>
+                Need help or suggest anything{" "}
+                <Link to="/" className="text-[#3c37ff] hover:underline">
+                  here
+                </Link>
+              </span>
             </div>
           </div>
         </div>
-        {/* left side end  */}
-
-        {/* right side start  */}
-        <div className="w-1/2 hidden md:block h-full">
-          <img src={loginImage} alt="" />
-        </div>
-
-        {/* right side end  */}
-        <ToastContainer
-          position="top-right"
-          autoClose={5000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme="light"
-        />
-
-        <ToastContainer />
       </div>
+      {/* left side end  */}
+
+      {/* right side start  */}
+      <div className="hidden md:block w-1/2 h-full">
+        <img src={loginImage} alt="" />
+      </div>
+      {/* right side end  */}
+
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+
+      <ToastContainer />
     </div>
   );
 };
